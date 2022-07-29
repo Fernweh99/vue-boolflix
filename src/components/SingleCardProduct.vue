a380be97b86cd814bfa8a2ee8d05d5c20915b7ac
Fernweh99 <daniele.dimento@gmail.com>
2022-07-29 16:01:34 +0200

    finish milestone 3

--- src/components/SingleCardProduct.vue
+++ src/components/SingleCardProduct.vue
@@ -6,7 +6,11 @@
       <p>{{ original_title }}</p>
       <img v-if="hasFlag" class="flag" :src="srcFlag" :alt="'flag '+language">
       <p v-else>{{ language }}</p>
-      <p>{{ getNumberForStars }}</p>
+      <div>
+        <svg v-for="(star , i) in maxRating" :key="i" xmlns="http://www.w3.org/2000/svg" width="16" height="16" :fill="isStar(i)" class="bi bi-star-fill" viewBox="0 0 16 16">
+          <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+        </svg>
+      </div>
     </div>
   </div>
 </template>
@@ -21,6 +25,11 @@ export default {
     vote: Number,
     poster: String,
   },
+  data() {
+    return {
+      maxRating: ["false", "false", "false", "false", "false"],
+    }
+  },
   computed: {
     hasFlag() {
       const flags = ["it", "en"];
@@ -34,7 +43,13 @@ export default {
       else return "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.svg.png"
     },
     getNumberForStars() {
-      return Math.ceil(Math.ceil(this.vote)/2)
+      return parseInt(Math.ceil(Math.ceil(this.vote)/2))
+    },
+  },
+  methods: {
+    isStar(i) {
+      if(i < this.getNumberForStars) return "black"
+      else return "white"
     }
   }
 }
